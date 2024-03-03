5ce58c0f247716de932bc6e5dfc5e11546726c24
Kid-Chang <pch3310@gmail.com>
2024-03-03 10:11:22 +0900

    더미데이터 제작

---
+++ src/mock/dummydata.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    id: 1,
+    news_url: "https://www.bbc.com/news/world-us-canada-60387997",
+    title: "US Supreme Court to hear major abortion case",
+    desc: "The court will consider a Mississippi law that bans most abortions after 15 weeks of pregnancy.",
+    img: "https://picsum.photos/400/300",
+  },
+  {
+    id: 2,
+    news_url: "https://www.bbc.com/news/world-asia-60387998",
+    title: "Myanmar junta pardons more than 23,000 prisoners",
+    desc: "The move comes as the military faces international pressure over its coup and crackdown on dissent.",
+    img: "https://picsum.photos/400/300",
+  },
+  {
+    id: 4,
+    news_url:
+      "https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=102&oid=001&aid=0013824425",
+    title: "윤 대통령, 민주당 '거리 통치' 비판…5·18민주화운동 기념식서",
+    desc: '윤석열 대통령이 18일 5·18민주화운동 38주년 기념식에서 민주당의 \'거리 통치\'를 비판했다. 윤 대통령은 이날 광주 민주화운동공원에서 열린 기념식 연설에서 "최근 몇 년 동안 거리 통치가 우리 사회의 새로운 정상으로 자리 잡는 듯한 분위기가 조성되고 있다"고 지적했다. \n\n윤 대통령은 "민주주의는 다수결의 원칙에 따라 운영되는 정치 체제이지만, 다수결의 원칙이 절대적인 것은 아니다"고 강조했다. 그는 "다수의 의견이 항상 옳은 것은 아니며, 소수의 의견이 옳을 수도 있다"고 말했다. \n\n윤 대통령은 또한 "거리 통치는 소수의 목소리가 다수의 목소리를 압도하는 행위"라고 비판했다. 그는 "거리 통치는 민주주의를 위협하는 행위이며, 용납될 수 없다"고 말했다.',
+    img: "https://picsum.photos/400/300",
+  },
+  {
+    id: 5,
+    news_url:
+      "https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=101&oid=022&aid=0003742971",
+    title: "'이태원 참사 유가족, 정부에 국가재난 지정 촉구…'눈물의 기도회'",
+    desc: '이태원 압사 사고 유가족들은 18일 오전 서울 종로구 세종문화회관 앞에서 \'이태원 참사 국가재난 지정 및 특별법 제정 촉구 기도회\'를 열고 정부에 국가재난 지정을 촉구했다. \n\n유가족들은 이날 기도회에서 "정부는 이태원 참사를 국가재난으로 지정하고, 피해자들을 국가재난 피해자로 인정해야 한다"고 주장했다. 그들은 또한 "이태원 참사의 진상을 밝히기 위한 특별법을 제정해야 한다"고 요구했다. \n\n기도회에는 유가족 약 100여 명이 참석했다. 유가족들은 현수막을 들고 "이태원 참사 국가재난 지정", "특별법 제정" 등을 외치며 정부의 조치를 촉구했다.',
+    img: "https://picsum.photos/400/300",
+  },
+  {
+    id: 6,
+    news_url:
+      "https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=102&oid=001&aid=0013823655",
+    title:
+      "민주당, 윤 대통령 '거리 통치' 비판에 \"국민 무시…국정농단 책임 물어야\"",
+    desc: "민주당은 18일 윤석열 대통령이 5·18민주화운동 기념식에서 민주당의 '거리 통치'를",
+  },
+];
+
+export default data;
