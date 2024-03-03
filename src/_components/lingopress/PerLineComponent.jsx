e8d4e4752726243403d0c0fd16c168799b8b814b
Kid-Chang <pch3310@gmail.com>
2024-03-03 16:18:56 +0900

    feat: 번역 후 확인 클릭시 나오는 모달 제작

--- src/_components/lingopress/PerLineComponent.jsx
+++ src/_components/lingopress/PerLineComponent.jsx
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
 import translateText from "../../utils/translateText";
+import { useState } from "react";
 
 const LineWrapper = styled.div`
-  width: 90%;
+  width: 85%;
 `;
 
 const OriginalLine = styled.p`
@@ -13,7 +14,7 @@ const ConvertLine = styled.textarea`
   border-radius: 1rem;
   padding: 0.1rem 1rem;
   margin-top: 1rem;
-  width: 95%;
+  width: 100%;
   border: 1px solid #000;
   height: 2.6rem;
   resize: none;
@@ -36,14 +37,31 @@ const CheckButton = styled.button`
   height: 3rem;
   border-radius: 30%;
   background-color: #fff;
+  border: 0.2rem solid #000;
+`;
+
+const VerifyBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+`;
+
+const VerifyButton = styled.button`
+  margin-top: 0.5rem;
+  border: 0.2rem solid #000;
+  border-radius: 30%;
+  margin-right: 0.5rem;
 `;
 
 const PerLineComponent = ({ line }) => {
+  const [isTranslated, setIsTranslated] = useState(false);
   const handleTranslate = () => {
     // 1. 번역
     const translatedText = translateText(line);
-    // 2. db에 번역한 기록 등록
-    console.log(translatedText);
+    // 2. db에 번역한 기록 등록 - 추후 인공지능 이용시.
+
+    // 3. 번역 확인 문구 출력
+    setIsTranslated(true);
   };
 
   return (
@@ -54,7 +72,21 @@ const PerLineComponent = ({ line }) => {
             <OriginalLine>{line}</OriginalLine>
             <ConvertLine></ConvertLine>
           </LineWrapper>
-          <CheckButton onClick={() => handleTranslate()}>확인하기</CheckButton>
+          <VerifyBox>
+            {isTranslated ? (
+              <div>
+                올바르게 번역했나요?
+                <div>
+                  <VerifyButton>O</VerifyButton>
+                  <VerifyButton>X</VerifyButton>
+                </div>
+              </div>
+            ) : null}
+
+            <CheckButton onClick={() => handleTranslate()}>
+              확인하기
+            </CheckButton>
+          </VerifyBox>
         </>
       ) : null}
     </LineOuterWrapper>
