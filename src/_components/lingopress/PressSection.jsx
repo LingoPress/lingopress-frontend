2e91c76a07deb6134be4b1bfb2c2d388440cf368
Kid-Chang <pch3310@gmail.com>
2024-03-10 21:22:10 +0900

    feat: 로그인 유무에 따라 조회되는 api 변화

--- src/_components/lingopress/PressSection.jsx
+++ src/_components/lingopress/PressSection.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import TranslateSection from "./TranslateSection";
 import styled from "@emotion/styled";
-import axios from "axios";
+import { useAtom } from "jotai/index";
+import { authAtom } from "../../atom/user";
+import { axiosPrivate, axiosPublic } from "../../utils/axiosMethod";
 
 const PressTitle = styled.h1`
   font-size: 1.3rem;
@@ -10,17 +12,28 @@ const PressTitle = styled.h1`
 const PressSection = () => {
   const props = useParams();
   const [pressData, setPressData] = useState([]);
+  const [authStatus, setAuthStatus] = useAtom(authAtom);
   useEffect(() => {
     // 뉴스정보 가져오기
     const responseData = async () => {
-      const result = await axios.get(
-        `${process.env.REACT_APP_BACKEND_API_URL}/api/v1/press/${props.press_id}`,
-      );
-      console.log(result.data.data);
-      setPressData(result.data.data);
+      let result;
+      if (authStatus.is_logged_in !== null) {
+        console.log(authStatus.is_logged_in);
+        if (authStatus.is_logged_in === false) {
+          result = await axiosPublic.get(
+            `${process.env.REACT_APP_BACKEND_API_URL}/api/v1/press/${props.press_id}`,
+          );
+        } else {
+          result = await axiosPrivate.get(
+            `${process.env.REACT_APP_BACKEND_API_URL}/api/v1/press/${props.press_id}`,
+          );
+        }
+        console.log(result.data.data);
+        setPressData(result.data.data);
+      }
     };
     responseData();
-  }, []);
+  }, [authStatus]);
   return (
     <div>
       <PressTitle>{pressData ? pressData.title : null}</PressTitle>
