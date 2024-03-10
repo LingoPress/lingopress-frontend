e824b31638d3f01559468e8ed4b5ee42ecd6fe72
Kid-Chang <pch3310@gmail.com>
2024-03-10 21:23:14 +0900

    fix: 기존에 originalContent와 translatedContent를 받아오는 방식을 json에 맞추도록 변경

--- src/_components/lingopress/TranslateSection.jsx
+++ src/_components/lingopress/TranslateSection.jsx
@@ -2,26 +2,30 @@ import { useEffect, useState } from "react";
 import PerLineComponent from "./PerLineComponent";
 
 const TranslateSection = ({ pressData }) => {
-  const [originalContent, setOriginalContent] = useState([]);
-  const [translatedContent, setTranslatedContent] = useState([]);
+  // const [originalContent, setOriginalContent] = useState([]);
+  // const [translatedContent, setTranslatedContent] = useState([]);
+  const [content, setContent] = useState([]);
   useEffect(() => {
     if (pressData) {
-      setOriginalContent(pressData.originalText);
-      setTranslatedContent(pressData.translatedText);
+      // setOriginalContent(pressData.originalText);
+      // setTranslatedContent(pressData.translatedText);
+      setContent(pressData.content);
     }
   }, [pressData]);
 
   return (
     <>
-      {originalContent ? (
+      {content ? (
         <div>
-          {originalContent.map((item, index) => (
+          {content.map((item, index) => (
             <PerLineComponent
               key={index}
               // lineNumber는 1부터 시작해야함.
-              lineNumber={index + 1}
-              originalContent={item}
-              translatedContent={translatedContent[index]}
+              lineNumber={item.pressContentLineNumber}
+              originalContent={item.originalLineText}
+              translatedContent={item.machineTranslatedLineText}
+              userTranslatedContent={item.userTranslatedLineText}
+              isCorrect={item.isCorrect}
             />
           ))}
         </div>
