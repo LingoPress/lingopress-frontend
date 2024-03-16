d34837ee41038b9ecd09889f5b2eb92996073be5
Kid-Chang <pch3310@gmail.com>
2024-03-16 13:33:59 +0900

    feat: 무한스크롤 구현

--- src/_components/home/PressSection.jsx
+++ src/_components/home/PressSection.jsx
@@ -1,20 +1,56 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import PressCard from "./PressCard";
 
 const PressSection = () => {
   const [pressData, setPressData] = useState([]);
+  const [isLast, setIsLast] = useState(false);
+  const [page, setPage] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleObserver = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting && !isLoading) {
+      setPage((currentPage) => currentPage + 1);
+    }
+  };
+
+
   useEffect(() => {
-    // 뉴스정보 가져오기
-    const responseData = async () => {
-      const result = await axios.get(
-        `${process.env.REACT_APP_BACKEND_API_URL}/api/v1/press`,
-      );
-      // console.log(result.data.data.content);
-      setPressData(result.data.data.content);
-    };
-    responseData();
+    const observer = new IntersectionObserver(handleObserver, {
+      threshold: 0,
+    });
+    const observerTarget = document.getElementById("observer");
+    if (observerTarget) {
+      observer.observe(observerTarget);
+    }
   }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  const fetchData = async () => {
+    if (isLast) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const result = await axios.get(
+      `${process.env.REACT_APP_BACKEND_API_URL}/api/v1/press`,
+      {
+        params: {
+          page: page,
+        },
+      },
+    );
+    setPressData((pressData) => [...pressData, ...result.data.data.content]);
+    setIsLast(result.data.data.last);
+    setIsLoading(false);
+
+  };
 
   return (
     <div>
@@ -23,6 +59,8 @@ const PressSection = () => {
           ? pressData.map((news) => <PressCard key={news.id} {...news} />)
           : null}
       </ul>
+      {isLoading && <p>Loading...</p>}
+      <div id="observer" style={{height: "10px"}}></div>
     </div>
   );
 };
