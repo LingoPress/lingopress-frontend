27d8248bbf04c487aaa5f252d2fb7ba3ac8e4d54
Kid-Chang <pch3310@gmail.com>
2024-03-23 14:42:29 +0900

    feat: authStatus 상태를 Lingopress 컴포넌트에서 관리하고 하위의 PressSection과 Vocabulary에 사용하는 방식으로 최적화. PressSection의 atom상태관리는 제거

--- src/_components/lingopress/PressSection.jsx
+++ src/_components/lingopress/PressSection.jsx
@@ -2,17 +2,14 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import TranslateSection from "./TranslateSection";
 import styled from "@emotion/styled";
-import {useAtom} from "jotai/index";
-import {authAtom} from "../../atom/user";
 import {axiosPrivate, axiosPublic} from "../../utils/axiosMethod";
 
 const PressTitle = styled.h1`
   font-size: 1.3rem;
 `;
-const PressSection = () => {
+const PressSection = ({authStatus}) => {
   const props = useParams();
   const [pressData, setPressData] = useState([]);
-  const [authStatus, setAuthStatus] = useAtom(authAtom);
   useEffect(() => {
     // 뉴스정보 가져오기
     const responseData = async () => {
--- src/_pages/lingopress/Lingopress.jsx
+++ src/_pages/lingopress/Lingopress.jsx
@@ -1,14 +1,18 @@
 import PressSection from "../../_components/lingopress/PressSection";
 import Vocabulary from "../../_components/lingopress/Vocabulary";
+import {useAtomValue} from "jotai/index";
+import {authAtom} from "../../atom/user";
 
 export default function Lingopress() {
+  const authStatus = useAtomValue(authAtom);
+
   return (
     <div style={{
       display: "flex",
     }}>
       <br/>
-      <PressSection/>
-      <Vocabulary/>
+      <PressSection authStatus={authStatus}/>
+      {authStatus ? <Vocabulary/> : null}
     </div>
   );
 }
