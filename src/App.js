29f406f65eeefdfa32861f2d42c492609405bf17
Kid-Chang <pch3310@gmail.com>
2024-04-10 20:51:22 +0900

    feat: ga4 분석도구 삽입

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import "./App.css";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./_pages/home/Home";
 import Lingopress from "./_pages/lingopress/Lingopress";
 import styled from "@emotion/styled";
-import {Layout} from "./_layout/defaultLayout";
+import { Layout } from "./_layout/defaultLayout";
 import Login from "./_pages/login/Login";
 import SignUp from "./_pages/signUp/SignUp";
 import MyLingopress from "./_pages/myLingopress/MyLingopress";
 import Vocabulary from "./_pages/vocabulary/Vocabulary";
+import RouteChangeTracker from "./utils/RouteChangeTracker";
 
 const NotFound = styled.div`
   text-align: center;
 `;
 
 function App() {
+  RouteChangeTracker();
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/lingopress/:press_id" element={<Lingopress/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/my-lingopress" element={<MyLingopress/>}/>
-        <Route path="/vocabulary" element={<Vocabulary/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/lingopress/:press_id" element={<Lingopress />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/my-lingopress" element={<MyLingopress />} />
+        <Route path="/vocabulary" element={<Vocabulary />} />
         <Route
           path={"*"}
           element={
             <>
               <NotFound>
-                404 <br/> NOT FOUND
+                404 <br /> NOT FOUND
               </NotFound>
             </>
           }
