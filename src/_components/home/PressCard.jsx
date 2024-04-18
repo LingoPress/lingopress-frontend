7edd43fcccf89ffb1c6d94793dd15e53905dfc0f
Kid-Chang <pch3310@gmail.com>
2024-04-18 22:37:48 +0900

    style: 뉴스 목록화면을 v2에 맞게 수정

--- src/_components/home/PressCard.jsx
+++ src/_components/home/PressCard.jsx
@@ -1,16 +1,22 @@
 import styled from "@emotion/styled";
-import {Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import formatDate from "../../utils/formatDate";
+import { customColors } from "../../styles/color";
 
 const PressCardWrapper = styled.div`
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease;
   display: flex;
-  height: 10rem;
-  background-color: whitesmoke;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  background-color: ${customColors.background.boxBg["0"]};
   overflow: hidden;
-  margin-bottom: 1.2rem;
+  width: 40rem;
+  // height: 45rem;
+  //margin-bottom: 10rem;
+  white-space: pre-line;
+  line-height: 2.3rem;
+  font-weight: 300;
 
   &:hover {
     transform: translateY(-5px);
@@ -19,66 +25,65 @@ const PressCardWrapper = styled.div`
 
 const ImageContainer = styled.div`
   overflow: hidden;
-  width: 15rem;
+  width: 40rem;
 `;
 
 const Image = styled.img`
   border-radius: 10px;
   width: 100%;
-  height: 10rem;
+  height: 25rem;
   object-fit: cover;
 `;
 
 const Content = styled.div`
-  padding: 20px;
   width: 100%;
 `;
 
 const Title = styled.h2`
-  margin-top: 15px;
-  font-size: 1.5rem;
+  margin-top: 2rem;
+  font-size: 2rem;
   color: #333;
   text-overflow: ellipsis;
+  color: ${customColors.text.cardTitle["100"]};
 `;
 
 const Description = styled.p`
-  margin-top: 10px;
+  margin-top: 1rem;
   line-height: normal;
-  color: #666;
+  color: ${customColors.text.subExplain["400"]};
   max-height: 6rem;
   white-space: normal;
   overflow: hidden;
-
+  font-size: 1.6rem;
   display: -webkit-box;
   -webkit-line-clamp: 3; /*보여줄 줄의 수를 정함*/
   -webkit-box-orient: vertical; /*box의 배열 방향을 정함*/
 `;
 
 const PublishedTime = styled.p`
-  margin-top: 10px;
+  margin-top: 1rem;
   color: #999;
-  font-size: 0.8rem;
+  font-size: 1.4rem;
   text-align: right;
 `;
 
-const PressCard = ({id, publishedAt, title, content, imageUrl}) => {
+const PressCard = ({ id, publishedAt, title, content, imageUrl }) => {
   const formattedDate = formatDate(publishedAt);
+  const navigate = useNavigate();
 
   return (
-    <Link to={`lingopress/${id}`}>
-      <PressCardWrapper>
-        {imageUrl ? (
-          <ImageContainer>
-            <Image src={imageUrl} alt={title}/>
-          </ImageContainer>
-        ) : null}
-        <Content>
-          <Title>{title}</Title>
-          <Description>{content}</Description>
-          <PublishedTime>{formattedDate}</PublishedTime>
-        </Content>
-      </PressCardWrapper>
-    </Link>
+    <PressCardWrapper onClick={() => navigate(`/lingopress/${id}`)}>
+      {imageUrl ? (
+        <ImageContainer>
+          <Image src={imageUrl} alt={title} />
+        </ImageContainer>
+      ) : null}
+      <Content>
+        <Title>{title}</Title>
+        <Description>{content}</Description>
+        <PublishedTime>{formattedDate}</PublishedTime>
+      </Content>
+    </PressCardWrapper>
   );
 };
 
