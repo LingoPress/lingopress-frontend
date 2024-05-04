7b20d6fdce39ad1b6f3c75d06bb22711e5d37084
Kid-Chang <pch3310@gmail.com>
2024-05-04 10:24:17 +0900

    feat: 뉴스 정렬 기준 선택가능 - date, line number - asc, desc

--- src/_components/home/PressSection.jsx
+++ src/_components/home/PressSection.jsx
@@ -2,12 +2,30 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import PressCard from "./PressCard";
 import styled from "@emotion/styled";
+import { Dropdown } from "react-bootstrap";
+
+const SortCriteriaType = [
+  ["publishedAt", "날짜"],
+  ["totalContentLine", "뉴스 문장 수"],
+];
+
+const SortOrderType = [
+  ["desc", "내림차순"],
+  ["asc", "오름차순"],
+];
 
 const PressSectionWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 5rem;
+`;
+
+const OrderOptionWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 2rem;
+  margin-bottom: 2rem;
 `;
 
 const PressSection = () => {
@@ -17,9 +35,13 @@ const PressSection = () => {
 
   const [isLoading, setIsLoading] = useState(false);
 
+  // 정렬 기준
+  const [sortCriteria, setSortCriteria] = useState(SortCriteriaType[0]);
+  const [sortType, setSortType] = useState(SortOrderType[0]);
+
   const handleObserver = (entries) => {
     const target = entries[0];
-    if (target.isIntersecting && !isLoading) {
+    if (target.isIntersecting && !isLoading && !isLast) {
       setPage((currentPage) => currentPage + 1);
     }
   };
@@ -34,22 +56,19 @@ const PressSection = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-
-  const fetchData = async () => {
+  const fetchData = async (pageValue, sortCriteria, sortType) => {
     if (isLast) {
       return;
     }
-
     setIsLoading(true);
 
     const result = await axios.get(
       `${process.env.REACT_APP_BACKEND_API_URL}/v1/press`,
       {
         params: {
-          page: page,
+          page: pageValue,
+          sort: sortCriteria[0],
+          order: sortType[0],
         },
       },
     );
@@ -58,15 +77,95 @@ const PressSection = () => {
     setIsLoading(false);
   };
 
-  return (
-    <PressSectionWrapper>
-      {pressData.length > 0
-        ? pressData.map((news) => <PressCard key={news.id} {...news} />)
-        : null}
+  useEffect(() => {
+    console.log(page);
+    fetchData(page, sortCriteria, sortType);
+  }, [page]);
 
-      {isLoading && <p>Loading...</p>}
-      <div id="observer" style={{ height: "10px" }}></div>
-    </PressSectionWrapper>
+  const searchParamChange = (type, value) => {
+    setPage(() => 0);
+    setPressData([]);
+    setIsLast(false);
+    if (type === "SortCriteriaType") {
+      console.log(value);
+      setSortCriteria(value);
+    }
+    if (type === "SortOrderType") {
+      setSortType(value);
+      console.log(value);
+    }
+  };
+
+  return (
+    <>
+      <OrderOptionWrapper>
+        <Dropdown>
+          <Dropdown.Toggle
+            size="lg"
+            variant="Secondary"
+            id="dropdown-basic"
+            style={{ marginRight: "1rem" }}
+          >
+            {sortCriteria[1]}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu
+            style={{
+              fontSize: "1.4rem",
+            }}
+          >
+            <Dropdown.Item
+              onClick={() =>
+                searchParamChange("SortCriteriaType", SortCriteriaType[0])
+              }
+            >
+              날짜
+            </Dropdown.Item>
+            <Dropdown.Item
+              onClick={() =>
+                searchParamChange("SortCriteriaType", SortCriteriaType[1])
+              }
+            >
+              뉴스 문장 수
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown>
+          <Dropdown.Toggle size="lg" variant="Secondary" id="dropdown-basic">
+            {sortType[1]}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu
+            style={{
+              fontSize: "1.4rem",
+            }}
+          >
+            <Dropdown.Item
+              onClick={() =>
+                searchParamChange("SortOrderType", SortOrderType[0])
+              }
+            >
+              내림차순
+            </Dropdown.Item>
+            <Dropdown.Item
+              onClick={() =>
+                searchParamChange("SortOrderType", SortOrderType[1])
+              }
+            >
+              오름차순
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </OrderOptionWrapper>
+      <PressSectionWrapper>
+        {pressData.length > 0
+          ? pressData.map((news) => <PressCard key={news.id} {...news} />)
+          : null}
+
+        {isLoading && <p>Loading...</p>}
+        <div id="observer" style={{ height: "10px" }}></div>
+      </PressSectionWrapper>
+    </>
   );
 };
 
