42a356a896206e67bbdae5ecc4d636bcdc6c5f47
Kid-Chang <pch3310@gmail.com>
2024-05-11 14:59:09 +0900

    feat: 언어 기능 추가 안내문구 추가

---
+++ src/_components/myPage/LanguageSetting.jsx
@@ -0,0 +1,16 @@
+const LanguageSetting = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+      }}
+    >
+      <h1>조만간 언어 변경 기능이 추가됩니다!</h1>
+    </div>
+  );
+};
+
+export default LanguageSetting;
--- src/_pages/myPage/MyPage.jsx
+++ src/_pages/myPage/MyPage.jsx
@@ -5,6 +5,8 @@ import { useMediaQuery } from "react-responsive";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import MyMemo from "../../_components/myPage/MyMemo";
+import LanguageSetting from "../../_components/myPage/LanguageSetting";
+import { axiosPrivate } from "../../utils/axiosMethod";
 
 const MyPageWrapper = styled.div`
   display: ${({ isMobile }) => (isMobile ? "block" : "flex")};
@@ -29,10 +31,12 @@ const MyNav = styled.nav`
   border-bottom: 1px solid #e9ecef;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
-const NavItem = styled.div`
+const NavItem = styled.a`
   font-size: 1.5rem;
   font-weight: 600;
   cursor: pointer;
+  color: black;
+  text-decoration: none;
 `;
 
 const MyPageContentWrapper = styled.div`
@@ -50,6 +54,23 @@ const MyPage = () => {
     }
   }, []);
 
+  const deleteUser = () => {
+    if (window.confirm("정말로 탈퇴하시겠습니까?")) {
+      axiosPrivate({
+        method: "patch",
+        url: "/v1/users/delete",
+      }).then(
+        (res) => {
+          console.log(res.data);
+        },
+        (err) => {
+          console.log(err.data);
+        },
+      );
+      ///      localStorage.removeItem("token");
+      // navigate("/");
+    }
+  };
   return (
     <MyPageWrapper isMobile={isMobile}>
       <MyNav isMobile={isMobile}>
@@ -62,14 +83,13 @@ const MyPage = () => {
         <NavItem onClick={() => navigate("/my-page/language")}>
           언어 설정
         </NavItem>
-        <NavItem onClick={() => navigate("/my-page/opinion")}>
-          의견 제시하기
-        </NavItem>
-        <NavItem onClick={() => navigate("/my-page/out")}>탈퇴하기</NavItem>
+        <NavItem href="mailto: dev.lingopress@gmail.com">의견 제시하기</NavItem>
+        <NavItem onClick={() => deleteUser()}>탈퇴하기</NavItem>
       </MyNav>
       <MyPageContentWrapper>
         {option === "my-lingopress" && <MyLingopress isMobile={isMobile} />}
         {option === "my-memo" && <MyMemo isMobile={isMobile} />}
+        {option === "language" && <LanguageSetting />}
       </MyPageContentWrapper>
     </MyPageWrapper>
   );
