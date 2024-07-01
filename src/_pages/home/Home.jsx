7890f911bfd329c4ee074beec445f8558f4cfa02
Kid-Chang <pch3310@gmail.com>
2024-07-01 18:12:35 +0900

    fix: 내용물이 많지 않을때 height가 너무 적어 푸터가 중앙에 배치되 문제 해결

--- src/_pages/home/Home.jsx
+++ src/_pages/home/Home.jsx
@@ -72,7 +72,7 @@ export default function Home() {
 
   const navigate = useNavigate();
   return (
-    <div>
+    <div style={{ minHeight: "85vh" }}>
       {/* 랜딩 모달 */}
       {/* 유저가 비 로그인 상태일 때, 그리고 오늘하루 보지않기 클릭시 로컬스토리지에 해당 값 저장 */}
       {/*  <LandingModal /> */}
