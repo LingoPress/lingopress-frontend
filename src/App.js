7b651d89055bdd51f85c20eea29a15d7fc16d295
Kid-Chang <pch3310@gmail.com>
2024-07-10 19:03:03 +0900

    feat: EnrollVideo 페이지 라우트에 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import RouteChangeTracker from "./utils/RouteChangeTracker";
 import LandingPage from "./_pages/landingPage/LandingPage";
 import OAuthLogic from "./_pages/login/OAuthLogic";
 import MyPage from "./_pages/myPage/MyPage";
+import EnrollVideo from "./_pages/EnrollVideo";
 
 const NotFound = styled.div`
   text-align: center;
@@ -23,6 +24,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:category" element={<Home />} />
+        <Route path="/enroll_video" element={<EnrollVideo />} />
         <Route path="/whatis" element={<LandingPage />} />
         <Route path="/lingopress/:press_id" element={<Lingopress />} />
         <Route path="/login" element={<Login />} />
